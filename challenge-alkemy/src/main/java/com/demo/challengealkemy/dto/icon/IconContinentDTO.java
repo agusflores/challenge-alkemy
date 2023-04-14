deb436f6c58ff1bddc3b1d6f6423981134f1bdaf
Agustin Flores <agustinflores1505@gmail.com>
2023-04-14 02:21:53 -0300

    IconContinentDTO creado

---
+++ challenge-alkemy/src/main/java/com/demo/challengealkemy/dto/icon/IconContinentDTO.java
@@ -0,0 +1,72 @@
+package com.demo.challengealkemy.dto.icon;
+
+import java.util.Date;
+
+public class IconContinentDTO {
+    private String iconImage;
+    private String iconDenomination;
+    private Date iconCreation;
+    private float iconHeight;
+    private String iconHistory;
+    private String iconContinent;
+
+    public IconContinentDTO() {
+    }
+
+    public IconContinentDTO(String iconImage, String iconDenomination, Date iconCreation, float iconHeight, String iconHistory, String iconContinent) {
+        this.iconImage = iconImage;
+        this.iconDenomination = iconDenomination;
+        this.iconCreation = iconCreation;
+        this.iconHeight = iconHeight;
+        this.iconHistory = iconHistory;
+        this.iconContinent = iconContinent;
+    }
+
+    public String getIconImage() {
+        return iconImage;
+    }
+
+    public void setIconImage(String iconImage) {
+        this.iconImage = iconImage;
+    }
+
+    public String getIconDenomination() {
+        return iconDenomination;
+    }
+
+    public void setIconDenomination(String iconDenomination) {
+        this.iconDenomination = iconDenomination;
+    }
+
+    public Date getIconCreation() {
+        return iconCreation;
+    }
+
+    public void setIconCreation(Date iconCreation) {
+        this.iconCreation = iconCreation;
+    }
+
+    public float getIconHeight() {
+        return iconHeight;
+    }
+
+    public void setIconHeight(float iconHeight) {
+        this.iconHeight = iconHeight;
+    }
+
+    public String getIconHistory() {
+        return iconHistory;
+    }
+
+    public void setIconHistory(String iconHistory) {
+        this.iconHistory = iconHistory;
+    }
+
+    public String getIconContinent() {
+        return iconContinent;
+    }
+
+    public void setIconContinent(String iconContinent) {
+        this.iconContinent = iconContinent;
+    }
+}
---
+++ challenge-alkemy/src/main/java/com/demo/challengealkemy/repository/IconContinentRepositoryImpl.java
@@ -0,0 +1,2 @@
+package com.demo.challengealkemy.repository;public class IconContinentRepositoryImpl {
+}
